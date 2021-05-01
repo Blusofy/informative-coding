38cbfa1f1f460c87eb4ed98888cb9b7288d448c6
dev-mdminhaj <developer.mdminhaj@gmail.com>
2021-05-01 22:06:09 +0000

    Deploying to gh-pages from @ InformativeCoding/informativecoding.github.io@8ce100eaa1fc8bbdb2cd3f26437c9e6c6664176b 🚀

---
+++ _next/static/kX2H--u36IHi6tNaW5U_C/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static\u002Fchunks\u002Fpages\u002Findex-fb0d6ab3fc9deda349d0.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-62794a0eed78c21bc229.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
